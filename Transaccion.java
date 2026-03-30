b662d730a4d513797fe5fe41a1cca72997ce225b
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2026-03-30 15:59:59 -0400

    feat: add mostrarDetalle method

--- Transaccion.java
+++ Transaccion.java
@@ -12,4 +12,13 @@ public class Transaccion {
         this.descripcion = descripcion;
         this.exitosa = true;
     }
+
+    public void mostrarDetalle() {
+        System.out.println("--- Detalle de Transacción ---");
+        System.out.println("Tipo: " + tipo);
+        System.out.printf("Monto: $%.2f%n", monto);
+        System.out.println("Descripción: " + descripcion);
+        System.out.println("Estado: " + (exitosa ? "Exitosa" : "Fallida"));
+    }
+    
 }
