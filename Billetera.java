66021ae7895977ef74aad27516bf4cb746e56218
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2026-03-30 15:07:09 -0400

    feat: agregar metodo mostrarInfo a Billetera

--- Billetera.java
+++ Billetera.java
@@ -1,5 +1,22 @@
 package examenFinal;
 
 public class Billetera {
-    
+    private String propietario;
+    private String numeroCuenta;
+    private double saldo;
+    private boolean activa;
+
+    public Billetera(String propietario, String numeroCuenta, double saldoInicial) {
+        this.propietario = propietario;
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldoInicial;
+        this.activa = true;
+    }
+    public void mostrarInfo() {
+        System.out.println("=== Billetera Digital ===");
+        System.out.println("Propietario: " + propietario);
+        System.out.println("Cuenta: " + numeroCuenta);
+        System.out.printf("Saldo: $%.2f%n", saldo);
+        System.out.println("Estado: " + (activa ? "Activa" : "Inactiva"));
+    }
 }
