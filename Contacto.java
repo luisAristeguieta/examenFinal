647045f303df77d3f1c977210d8ff4246f608483
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2026-03-30 16:39:24 -0400

    feat: add attribute with constructor

--- Contacto.java
+++ Contacto.java
@@ -2,5 +2,15 @@
 package examenFinal;
 
 public class Contacto {
+    private String nombre;
+    private String cuentaDestino;
+    private String banco;
+    private boolean favorito;
 
+    public Contacto(String nombre, String cuentaDestino, String banco) {
+        this.nombre = nombre;
+        this.cuentaDestino = cuentaDestino;
+        this.banco = banco;
+        this.favorito = false;
+    }
 }
